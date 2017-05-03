6d2b59786aab171a0dfcc8f94c341e4d9a172dc5
David Sawatske <sawatskeda10@gmail.com>
2017-05-02 21:09:29 -0700

    Work on Section 3 (lessons 17-19)

---
+++ 3 - Types and Operators/17_to_19.js
@@ -0,0 +1,25 @@
+// 17 //
+
+// Dynamic Typing - you don't tell the engine what type of data the var holds
+//  => it's figured out during execution
+//  => can hold different types depending on what is being executed
+
+
+// 18 //
+
+// Primitive Type - a type of data that represents a single value (not an object)
+// 1. undefined - lack of existence/ never set (what JS engine sets vars to initially)
+// 2. null - lack of existence (used when you want to set var to nothing)
+// 3. boolean - true or false
+// 4. number - floating point (ie decimals) is the only type in JS
+// 5. string - sequence of chars with '' or ""
+// 6. symbol - used in ES6
+
+
+// 19 //
+
+// Operators - a special function that is sytactically different
+//  => Generally, they take two parameters and return one result
+
+var a = 3 + 4;   // the addition operator (function) is called with infix notation
+console.log(a);
